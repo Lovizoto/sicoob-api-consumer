dd7ea7cf1f4a9b1d85897708359c1a99df5268a8
Lovizoto <lovizoto@gmail.com>
2025-08-30 19:28:53 -0300

    update project

--- src/main/java/br/com/lovizoto/integracaosicoob/APISicoobV3.java
+++ src/main/java/br/com/lovizoto/integracaosicoob/APISicoobV3.java
@@ -38,6 +38,8 @@ public class APISicoobV3 {
                 String tokenCobranca = sicoobAuth.getAuthTokenForCobranca().getAccessToken();
                 
                 
+               
+                
                 
                 
             
